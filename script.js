62b573e53cfb851c91ab2de13e429a7c21de4c88
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2020-05-15 15:06:24 -0300

    Usando o metodo Ajax

--- script.js
+++ script.js
@@ -20,4 +20,24 @@ $(document).ready(() =>{
 			$('#pagina').html(data)
 			})
 	})
+
+
+	///metodo ajax
+	$('#competencia').on('change',e=>{
+		
+		let competencia = $(e.target).val()
+		
+		///definir o metodo , url , o que acontece se tive sucesso e tiver erro
+		$.ajax({
+			type: 'GET',
+			url: 'app.php',
+			data: `competencia=${competencia}`,
+			dataType:'json',
+			success: dados =>{
+				$('#numeroVendas').html(dados.numeroVendas)
+				$('#totalVendas').html(dados.totalVendas)
+			} ,
+			error: erro =>{console.log(erro)}
+		})
+	})
 })
